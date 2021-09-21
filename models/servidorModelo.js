5c8cf3984093fdd24597e13a3b5365f9e32e93be
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2021-09-21 16:42:18 -0500

    Initial commit

---
+++ models/servidorModelo.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const {conectarBD}=require("../database/conexion.js")
+
+class SevidorModelo{
+
+
+    constructor(){
+
+        this.app = express();
+        this.levantarBD();
+        this.llamarRutas();
+
+
+    }
+
+    levantarSevidor(){
+
+        this.app.listen(process.env.PUERTO,function(){
+            console.log("servidor encendido "+process.env.PUERTO);
+        });
+
+    }
+
+    levantarBD(){
+        conectarBD();
+    }
+
+    llamarRutas(){
+
+        this.app.get('/facturas', function (req, res) {
+            res.send('Buenas tardes Juan Jose');
+        });
+
+        this.app.post('/facturas/nuevo', function (req, res) {
+            res.send('Buenas tardes Juan Jose');
+        });
+
+        this.app.put('/facturas/cambiar', function (req, res) {
+            res.send('Buenas tardes Juan Jose');
+        });
+        
+        this.app.delete('/facturas/borrar', function (req, res) {
+            res.send('Buenas tardes Juan Jose');
+        });
+
+    }
+
+
+
+
+
+}
+
+module.exports=SevidorModelo;
